ae517c9ac7031c4a9c1018731c5867fd5fa06b97
musliadi adi <hello.musliadi@gmail.com>
2021-09-21 13:11:29 +0700

    initial project

---
+++ pages/home.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <h1>Halaman Home</h1>
+    <NuxtLink to="/">
+        Ke index
+    </NuxtLink>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h1 class="red">Halaman Index</h1>
+    <NuxtLink to="/home">
+        Ke Home
+    </NuxtLink>
+  </main>
+</template>
+
+<script>
+  export default {
+    head() {
+      // Set Meta Tags for this Page
+    }
+    // ...
+  }
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
